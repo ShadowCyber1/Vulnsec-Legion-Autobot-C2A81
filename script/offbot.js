ae8f1c050260d15a9bc614167a3655cb7b47a9ec
ShadowCyber1 <149857164+ShadowCyber1@users.noreply.github.com>
2024-07-01 21:19:46 +0800

    Create offbot.js

---
+++ script/offbot.js
@@ -0,0 +1,12 @@
+module.exports.config = {
+    name: "offbot",
+    version: "1.1.0",
+    hasPermssion: 0,
+    credits: "yukihirasoma",
+    description: "Turn off the bot",
+    usePrefix: true,
+    commandCategory: "system",
+    cooldowns: 2,
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage("The bot is now turning off.", event.threadID, () => process.exit(0));
