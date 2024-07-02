cf226d979b6ee03c19dddba021b475ad687f46ec
ShadowCyber1 <149857164+ShadowCyber1@users.noreply.github.com>
2024-07-02 16:06:21 +0800

    Update out.js

--- script/out.js
+++ script/out.js
@@ -10,7 +10,21 @@ module.exports.config = {
     cooldowns: 2,
 };
 
-module.exports.run = async function({ api, event, args }) {
-    if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-    if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+module.exports.run = async function({ api, event, args, Users }) {
+    const userID = event.senderID;
+    const userInfo = await Users.getData(userID);
+
+    // Check if the user has admin privileges
+    if (!userInfo || userInfo.role < 2) {
+        return api.sendMessage("You do not have permission to use this command.", event.threadID);
+    }
+
+    if (!args[0]) {
+        return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+    }
+    if (!isNaN(args[0])) {
+        return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+    }
+
+    return api.sendMessage("Invalid command usage.", event.threadID);
 };
