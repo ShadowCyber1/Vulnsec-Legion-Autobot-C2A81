b2159be1f81065a9a8b0046b504eeaaafe7ce792
ShadowCyber1 <149857164+ShadowCyber1@users.noreply.github.com>
2024-07-02 16:09:49 +0800

    Update onbot.js

--- script/onbot.js
+++ script/onbot.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 module.exports.config = {
     name: "onbot",
     version: "1.1.0",
-    role: 2,
+    role: 2, // Indicates this command requires admin privileges
     credits: "yukihirasoma",
     description: "Turn on the bot",
     usePrefix: true,
@@ -11,7 +11,15 @@ module.exports.config = {
     cooldowns: 2,
 };
 
-module.exports.run = ({ event, api }) => {
+module.exports.run = async ({ event, api, Users }) => {
+    const userID = event.senderID;
+    const userInfo = await Users.getData(userID);
+
+    // Check if the user has admin privileges
+    if (!userInfo || userInfo.role < 2) {
+        return api.sendMessage("You do not have permission to use this command.", event.threadID);
+    }
+
     api.sendMessage("The bot is now turning on.", event.threadID, () => {
         exec('pm2 start bot', (error, stdout, stderr) => {
             if (error) {
