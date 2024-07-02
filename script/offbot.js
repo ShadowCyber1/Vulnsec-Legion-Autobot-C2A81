7aca6b67421185bbaf2a33b3af5db414b01b8cf8
ShadowCyber1 <149857164+ShadowCyber1@users.noreply.github.com>
2024-07-02 16:07:12 +0800

    Update offbot.js

--- script/offbot.js
+++ script/offbot.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "offbot",
     version: "1.1.0",
-    role: 2,
+    role: 2, // Indicates this command requires admin privileges
     credits: "yukihirasoma",
     description: "Turn off the bot",
     usePrefix: true,
@@ -9,6 +9,14 @@ module.exports.config = {
     cooldowns: 2,
 };
 
-module.exports.run = ({ event, api }) => {
+module.exports.run = async ({ event, api, Users }) => {
+    const userID = event.senderID;
+    const userInfo = await Users.getData(userID);
+
+    // Check if the user has admin privileges
+    if (!userInfo || userInfo.role < 2) {
+        return api.sendMessage("You do not have permission to use this command.", event.threadID);
+    }
+
     api.sendMessage("The bot is now turning off.", event.threadID, () => process.exit(0));
 };
